812be7e5b3114727780c501e2ebc02c64922f9cf
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-05-23 15:40:44 +0200

    Add Footer

--- src/Page/EmailForm.tsx
+++ src/Page/EmailForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 
-export default function EmailField({ title, description }: { title: string; description: string }) {
+export function EmailFieldYellow({ title, description }: { title: string; description: string }) {
   return (
     <Formik initialValues={{ email: '' }} onSubmit={console.log}>
-      <Form className="mx-auto">
-        <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <Form className="mx-auto max-w-xl">
+        <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-lg mb-4 text-gray-800">{description}</p>
         <div className="flex w-full flex-row">
           <Field
@@ -19,6 +19,31 @@ export default function EmailField({ title, description }: { title: string; desc
           </button>
         </div>
         <p className="text-xs uppercase text-gold-darkest mt-2 tracking-wider">
+          Posíláme maximálně jeden mail měsíčně
+        </p>
+      </Form>
+    </Formik>
+  );
+}
+
+export function EmailFieldGreen({ title, description }: { title: string; description: string }) {
+  return (
+    <Formik initialValues={{ email: '' }} onSubmit={console.log}>
+      <Form className="mx-auto max-w-xl">
+        <h2 className="text-2xl font-bold mb-2">{title}</h2>
+        <p className="text-lg mb-4 text-gray-800">{description}</p>
+        <div className="flex w-full flex-row">
+          <Field
+            className="flex-grow rounded-l-md px-4 py-2 text-lg text-black bg-green-100 placeholder-forest"
+            type="email"
+            name="email"
+            placeholder="jan.novak@seznam.cz"
+          />
+          <button type="submit" className="bg-green-600 px-4 py-2 rounded-r-md text-white">
+            Potvrdit
+          </button>
+        </div>
+        <p className="text-xs uppercase text-green-900 mt-2 tracking-wider">
           Posíláme maximálně jeden mail měsíčně
         </p>
       </Form>
--- src/Page/Footer.tsx
+++ src/Page/Footer.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import c from 'classnames';
+
+import olymp from '../assets/olymp-footer.svg';
+
+import { EmailFieldGreen } from './EmailForm';
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url('${olymp}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPositionX: '75%',
+        marginTop: '-2em',
+      }}
+      className="py-48 px-32 min-h-screen-3/4 flex flex-col content-center"
+    >
+      <div className="max-w-lg mb-12">
+        <EmailFieldGreen
+          title="Zaujal vás Olymp?"
+          description="Přihlašte se k odběru novinek o Olympu a my vám průběžně budeme dávat vědět, jak probíhá jeho vývoj."
+        />
+      </div>
+
+      <div className="max-w-lg">
+        <h2 className="text-2xl font-bold mb-2">Máte nápad, jak Olymp zlepšit?</h2>
+        <p className="text-lg mb-4 text-gray-800">
+          Všechny své nápady, návrhy a připomínky posílejte prosím na adresu{' '}
+          <a
+            className="font-bold cursor-pointer hover:text-green-900"
+            href="mailto:napady@getolymp.com"
+          >
+            napady@getolymp.com
+          </a>
+          . Díky!
+        </p>
+      </div>
+    </div>
+  );
 }
