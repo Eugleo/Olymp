b52659197485ca92b5bfa1c78a0d2c9f7b469351
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-05-23 20:51:38 +0200

    Add emails

--- src/Page/EmailForm.tsx
+++ src/Page/EmailForm.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 
+import emailjs from 'emailjs-com';
+
+function submitEmail(values: { email: string }) {
+  emailjs.send('gmail', 'template_Z2J36vTg', values, 'user_jxxm36Q8serIbYwo7Qc6S').then(
+    () => {
+      alert('Podařilo se! Děkujeme!');
+    },
+    () => {
+      alert('Vyskytla se nějaká chyba. Zkusíte to znovu, prosím?');
+    },
+  );
+}
+
 export function EmailFieldYellow({ title, description }: { title: string; description: string }) {
   return (
-    <Formik initialValues={{ email: '' }} onSubmit={console.log}>
+    <Formik initialValues={{ email: '' }} onSubmit={submitEmail}>
       <Form className="mx-auto max-w-xl">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-lg mb-4 text-gray-800">{description}</p>
         <div className="flex flex-row">
           <Field
+            required
             className="flex-grow rounded-l-md px-4 py-2 text-lg text-black bg-gold-dark placeholder-gold-darkest"
             type="email"
             name="email"
@@ -28,12 +42,13 @@ export function EmailFieldYellow({ title, description }: { title: string; descri
 
 export function EmailFieldGreen({ title, description }: { title: string; description: string }) {
   return (
-    <Formik initialValues={{ email: '' }} onSubmit={console.log}>
+    <Formik initialValues={{ email: '' }} onSubmit={submitEmail}>
       <Form className="mx-auto max-w-xl">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-lg mb-4 text-gray-800">{description}</p>
         <div className="flex w-full flex-row">
           <Field
+            required
             className="flex-grow rounded-l-md px-4 py-2 text-lg text-black bg-green-100 placeholder-forest"
             type="email"
             name="email"
